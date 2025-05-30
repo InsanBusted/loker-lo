9392f84081ff6536e9a185f4a8835134456845d5
insan <insanbusted@gmail.com>
2025-05-31 03:27:57 +0700

    update

--- components/SearchJobs/ExploreJobs/page.tsx
+++ components/SearchJobs/ExploreJobs/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
 const ExploreJobs = () => {
-  const [search, setSearch] = useState("");
+  // const [search, setSearch] = useState("");
   return (
     <section className="w-full px-10 xl:pt-40 pt-35 bg-white">
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between gap-10">
